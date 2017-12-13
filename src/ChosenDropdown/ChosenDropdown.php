919e464c4c5ba2bd87bffefd30e4f52f383507a0
robmadeyou <robmadeyou@gmail.com>
2017-12-13 11:07:14 +0000

    Renamed the leaf, and some syntax updates

--- src/ChosenDropdown/ChosenDropdown.php
+++ src/ChosenDropdown/ChosenDropdown.php
@@ -4,7 +4,8 @@ namespace Rhubarb\Leaf\Chosen\ChosenDropdown;
 
 use Rhubarb\Leaf\Controls\Common\SelectionControls\DropDown\DropDown;
 
-class ChosenDropdown extends DropDown {
+class ChosenDropdown extends DropDown
+{
     protected function getViewClass()
     {
         return ChosenDropdownView::class;
