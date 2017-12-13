01d3c618da02e3f9366a49ec14aaa5375e1a8a65
robmadeyou <robmadeyou@gmail.com>
2017-12-13 11:40:07 +0000

    Added styles, with the single working selector

--- src/ChosenDropdown/ChosenDropdownView.php
+++ src/ChosenDropdown/ChosenDropdownView.php
@@ -9,9 +9,15 @@ class ChosenDropdownView extends DropDownView
     public function getDeploymentPackage()
     {
         $package = parent::getDeploymentPackage();
-        $package->resourcesToDeploy[] = __DIR__ .'/../../vendor/components/jquery/jquery.min.js';
+        $package->resourcesToDeploy[] = VENDOR_DIR .'/components/jquery/jquery.min.js';
         $package->resourcesToDeploy[] = __DIR__ .'/../chosen.jquery.min.js';
+        $package->resourcesToDeploy[] = __DIR__ .'/../chosen.min.css';
         $package->resourcesToDeploy[] = __DIR__ . '/ChosenDropDownViewBridge.js';
         return $package;
+    }
+
+    protected function getViewBridgeName()
+    {
+        return 'ChosenDropDownViewBridge';
     }
 }
